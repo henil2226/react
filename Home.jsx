49815642b6023bdfe72ba8579fe157c226878ee4
henil2226 <henilpatel2226@gmail.com>
2023-10-07 20:05:30 +0530

    home.jsx

---
+++ Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+
+function Home() {
+    return (
+        <>
+           {/* <Navbar/> */}
+        <h1>home page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, non natus. Consequatur, at! Impedit veritatis architecto, tempore accusamus mollitia accusantium cum molestiae suscipit earum. Ducimus!</p>
+
+        {/* <Outlet></Outlet> */}
+        </>
+
+    )
+}
+
+
+export default Home
