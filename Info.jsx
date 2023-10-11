8030d7919afcb3a1941dbbba31af20dbba816f3a
henil2226 <henilpatel2226@gmail.com>
2023-10-11 19:24:12 +0530

    info.jsx

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+    return (
+        <>
+        <h1>Info page</h1>
+        <p>Lorem ipsum dolor sit amet ct. Possimus, non natus. Consequatur, at! Impedit veritatis architecto, tempore accusamus mollitia accusantium cum molestiae suscipit earum. Ducimus!</p>
+        </>
+    )
+}
+
+export default Info
