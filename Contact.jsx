173b999bc503d1825f7223fef8e19257f82a79f6
henil2226 <henilpatel2226@gmail.com>
2023-10-14 19:17:46 +0530

    contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <>
+        <h1>Contact page</h1>
+        <p>Lorem ipsum dolor sit  Consequatur, at! Impedit veritatis architecto, tempore accusamus mollitia accusantium cum molestiae suscipit earum. Ducimus!</p>
+        </>
+    )
+}
+
+export default Contact
