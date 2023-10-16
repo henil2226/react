431461b3cf0ad31070d0d79ecf423887891b6672
henil2226 <henilpatel2226@gmail.com>
2023-10-16 12:37:30 +0530

    about.jsx

---
+++ About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return (
+        <>
+        <h1>About  page</h1>
+        <p>ctetur adipisicing elit. Possimus, non natus. Consequatur, at! Impedit veritatis architecto, tempore accusamus mollitia accusantium cum molestiae suscipit earum. Ducimus!</p>
+        </>
+    )
+}
+
+export default About
