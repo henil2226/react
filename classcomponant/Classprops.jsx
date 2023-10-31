974504e572f398329c1fcb36bef69e11f6d523c9
henil2226 <henilpatel2226@gmail.com>
2023-10-31 12:13:22 +0530

    props.jsx

---
+++ classcomponant/Classprops.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+import Classcompointro from "./Classcompointro";
+
+class Classprops extends Component {
+    render() { 
+        return (
+           <>
+           <h1>class props</h1>
+           <Classcompointro change=" +  new class propss"  src="https://picsum.photos/id/237/200/300" />
+           </>
+        );
+    }
+}
+ 
+export default Classprops ;
